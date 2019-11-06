a73cd7a109ff72f28fa69e6314f4c2691837d978
gitnehakumari <nehak8297@gmail.com>
2019-11-06 13:28:58 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-map(x => x * x)(of(1, 2, 3)).subscribe((v) => console.log(`value: ${v}`));
+map(x => x * x)(of(1, 2, 3,4,5)).subscribe((v) => console.log(`value: ${v}`));
 
