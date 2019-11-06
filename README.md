d160f8d757a92c15810d33ddba78db4eebf0f6cf
gitnehakumari <nehak8297@gmail.com>
2019-11-06 13:27:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# uexzy1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/uexzy1)
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+map(x => x * x)(of(1, 2, 3)).subscribe((v) => console.log(`value: ${v}`));
+
